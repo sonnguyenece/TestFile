69533cdaf306af1460444cb91c1e681202877f70
nmson <nmson@ecpay.vn>
2020-10-22 17:15:52 +0700

    filterMonday.sql

---
+++ SQL/filterMonday.sql
@@ -0,0 +1,18 @@
+WITH lASTEST_MONDAY AS (
+    SELECT (CASE
+                WHEN (TO_CHAR(DAY, 'DY') = 'MON') THEN TO_CHAR(DAY, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'TUE') THEN TO_CHAR(DAY - 1, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'WED') THEN TO_CHAR(DAY - 2, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'THU') THEN TO_CHAR(DAY - 3, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'FRI') THEN TO_CHAR(DAY - 4, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'SAT') THEN TO_CHAR(DAY - 5, 'yyyy-mm-dd DY')
+                WHEN (TO_CHAR(DAY, 'DY') = 'SUN') THEN TO_CHAR(DAY - 6, 'yyyy-mm-dd DY')
+        END) MONDAY,
+           DAY,
+           VALUE
+    FROM BILL
+)
+SELECT MONDAY, SUM(VALUE) weekly_value
+FROM lASTEST_MONDAY
+group by MONDAY
+order by MONDAY desc;
