2a9e3b166bb3866a260f26a4aa1b0fbe67de727a
nmson <nmson@ecpay.vn>
2020-10-24 12:00:22 +0700

    pl/sql example

---
+++ SQL/PL_SQL.sql
@@ -0,0 +1,16 @@
+
+declare
+    VIS_ROW_ID    number ;
+    inputID number:=1;
+begin
+    Select ID
+    INTO VIS_ROW_ID
+    from BILL
+--     where ID=inputID
+    ;
+Exception when too_many_rows then
+    Dbms_Output.put_line('Error:'||Sqlerrm);
+End;
+
+
+
