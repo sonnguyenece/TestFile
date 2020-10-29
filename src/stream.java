c6b4a9d0202f55145025d7ab8070954cb39ef3bc
nmson <nmson@ecpay.vn>
2020-10-29 11:19:42 +0700

    stream filter map collect count

---
+++ src/stream.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class stream {
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("abcv", "xaca", "asdqsd", "scqgq");
+        List<String> upperCase = items.stream()
+                .map(item -> item.toUpperCase())
+                .collect(Collectors.toList());
+
+        List<String> mapper = items.stream()
+                .filter(item -> item.startsWith("a"))
+                .collect(Collectors.toList());
+
+        long count = items.stream()
+                .filter(item->item.contains("a"))
+                .count()
+                ;
+        System.out.println(count);
+
+    }
+
+}
