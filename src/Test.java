9a711bae8dfe4829c70b252f854fbc07e2cd5091
nmson <nmson@ecpay.vn>
2020-11-13 14:47:04 +0700

    JumpingCloud

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
-    static int shit;
+    static int jumpingOnClouds(int[] c) {
+        int jumpCount = 0;
+        int countZero = 0;
+        for (int i = 0; i < c.length - 1; i++) {
+            if (c[i] == 0) {
+                countZero++;
+            }
 
-    public void eat() {
+            if (c[i] == 0 && c[i + 1] == 1) {
+                jumpCount += countZero / 2;
+                countZero = 0;
+            }
 
+            if (c[i] == 1 && c[i + 1] == 0) {
+                jumpCount++;
+            }
+
+            if ((i == c.length - 2) && c[c.length - 2] == 0 && c[c.length - 1] == 0) {
+                jumpCount += (countZero + 1) / 2;
+                countZero = 0;
+            }
+        }
+        return jumpCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0}));
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0, 1}));
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0, 1, 1, 1, 0}));
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0}));
     }
 }
---
+++ src/testParse.java
@@ -0,0 +1,7 @@
+public class testParse {
+    public static void main(String[] args) {
+        String test=null;
+        System.out.println(Long.valueOf(test));
+        System.out.println(Long.parseLong(test));
+    }
+}
